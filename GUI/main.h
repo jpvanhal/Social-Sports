46c378c42b7090937ed42dfb302cb87c65e3a5c6
Kristoffer Snabb <kristoffer@Kristoffer-Snabbs-MacBook-Pro.local>
2010-12-13 19:10:16 +0200

    news tab added to the tabs

---
+++ GUI/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <QMainWindow>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAIN_H
